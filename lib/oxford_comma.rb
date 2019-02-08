b242c101974bda21356779776dbdd50331bde572
funny-reference-4499 <davidbernier91@gmail.com>
2019-02-08 15:38:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
 
+  def oxford_comma(array)
+
+  return array.join(' and ') if array.size == 2
+
+    if 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+   array.join(", ")
 end
