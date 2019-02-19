7cf1890f6d017357e5f68fa8d7b8d96cf95eebd0
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-19 05:08:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+  str = ""
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    str = array[0] + " and " + array[1]
+    return str
+  elsif array.size == 3
+    str = array[0] + ", " + array[1] + ", and " + array[2]
+    return str
+  else
+    array.each do |ele|
+      if ele == array[-1]
+        str << "and #{ele}"
+        return str
+      end
+      str << "#{ele}, "
+    end
+    return str
+  end
 end
